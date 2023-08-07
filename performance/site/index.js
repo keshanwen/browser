c69f5bdd14f5732dbd93509aa98fa18e656e2f25
keshanwen <3640@winnermedical.com>
2023-08-07 15:22:32 +0800

    feat:FP FCP 优化

--- performance/site/index.js
+++ performance/site/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const logger = require('morgan');
+const compression = require('compression')
 const delayConfig = require('./delayConfig');
 const app = express();
 app.use(logger('dev'));
+
 app.use((req, res, next) => {
     let url = req.url;
     let delay = delayConfig[url];
@@ -12,5 +14,11 @@ app.use((req, res, next) => {
         next();
     }
 });
-app.use(express.static('public'));
+app.use(compression());
+app.use(express.static('public', {
+    setHeaders
+}));
+function setHeaders(res, path) {
+    res.setHeader('cache-control', 'no-cache')
+}
 app.listen(80, () => console.log(`server started at 80`));
